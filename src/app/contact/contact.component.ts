39fed192956dd8cd82d0e0cd8b5ed4d0eef97bc1
javi <jaravifa@hotmail.com>
2024-01-14 16:39:59 +0100

    Setear y disable

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,20 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  formularioContacto: FormGroup;
+  usuarioActivo: any = {
+    nombre: 'Sergio',
+    apellido: 'Gomez',
+    dni: '12345678'
+  };
 
- 
-  public usuario: any = {
-    nombre: '',
-    email: ''
+  constructor(private form: FormBuilder) {
+    this.formularioContacto = this.form.group({
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      apellido: [''],
+      dni: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+    });
   }
 
-  enviar(){
-    console.log(this.usuario);
+  ngOnInit(): void {
+      this.formularioContacto.get('apellido')?.setValidators([Validators.required, Validators.minLength(3)]);
+
+      this.formularioContacto.get('apellido')?.clearValidators();
+      this.formularioContacto.get('apellido')?.updateValueAndValidity();
+
+      //this.formularioContacto.get(`nombre`)?.setValue(this.usuarioActivo);
+      this.formularioContacto.patchValue({
+        nombre: this.usuarioActivo.nombre,
+        apellido: this.usuarioActivo.apellido,
+        dni: this.usuarioActivo.dni
+      })
+      this.formularioContacto.get('nombre')?.disable();
+      this.formularioContacto.get('dni')?.disable();
   }
 
+  hasErrors( controlName: string , errorType: string){
+    return this.formularioContacto.get(controlName)?.hasError(errorType) && this.formularioContacto.get(controlName)?.touched
+  }
+  enviar() {
+    console.log(this.formularioContacto);
+  }
 }
